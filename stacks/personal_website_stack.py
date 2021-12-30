8792eed8d2070c3e49176fabbe05f2d775b60612
Benjamin Groseclose <ben.groseclose01@gmail.com>
2021-12-29 19:51:26 -0500

    add route53

--- stacks/personal_website_stack.py
+++ stacks/personal_website_stack.py
@@ -2,7 +2,10 @@ from aws_cdk import (
     core as cdk,
     aws_s3 as s3,
     aws_s3_deployment as s3_deployment,
-    aws_cloudfront as cloudfront
+    aws_cloudfront as cloudfront,
+    aws_route53 as route53,
+    aws_route53_targets as targets,
+    aws_certificatemanager as acm
 )
 
 class PersonalWebsiteStack(cdk.Stack):
@@ -10,10 +13,27 @@ class PersonalWebsiteStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        domain_name = 'bengroseclose.com'
+
+        hosted_zone = route53.HostedZone(
+            self,
+            f'{construct_id}-hosted-zone',
+            zone_name=domain_name,
+            comment=f'{domain_name} hosted zone'
+        )
+
+        cert = acm.DnsValidatedCertificate(
+            self,
+            f'{construct_id}-cert',
+            hosted_zone=hosted_zone,
+            region='us-east-2',
+            domain_name=domain_name
+        )
+
         bucket = s3.Bucket(
             self,
             f'{construct_id}-react-bucket',
-            bucket_name='ben-groseclose-website',
+            bucket_name=domain_name,
             public_read_access=True,
             removal_policy=cdk.RemovalPolicy.DESTROY,
             website_index_document='index.html'
@@ -26,7 +46,7 @@ class PersonalWebsiteStack(cdk.Stack):
             sources=[s3_deployment.Source.asset('code/ui/ben-groseclose-ui/build')]
         )
 
-        cloudfront.CloudFrontWebDistribution(
+        distribution = cloudfront.CloudFrontWebDistribution(
             self,
             f'{construct_id}-cloudfront',
             origin_configs=[
@@ -37,4 +57,12 @@ class PersonalWebsiteStack(cdk.Stack):
                     behaviors=[cloudfront.Behavior(is_default_behavior=True)]
                 )
             ]
+        )
+
+        route53.ARecord(
+            self,
+            f'{construct_id}-arecord',
+            zone=hosted_zone,
+            record_name=domain_name,
+            target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution))
         )
