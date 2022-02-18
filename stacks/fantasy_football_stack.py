44f2d82da54f19085ca2ad719fdf94f6fc03a8ae
Benjamin Groseclose <ben.groseclose01@gmail.com>
2022-02-17 21:09:16 -0500

    fix cdk

--- stacks/fantasy_football_stack.py
+++ stacks/fantasy_football_stack.py
@@ -8,9 +8,9 @@ class FantasyFootballStack(cdk.Stack):
 	def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
 		super().__init__(scope, construct_id, **kwargs)
 
-		table = dynamodb.Table(
-			self,
-			f'{construct_id}-dyanamodb',
-			table_name='FantasyFootball',
-			partition_key='Key'
-		)
+		# table = dynamodb.Table(
+		# 	self,
+		# 	f'{construct_id}-dyanamodb',
+		# 	table_name='FantasyFootball',
+		# 	partition_key='Key'
+		# )
