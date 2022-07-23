62e3f5bf72e0f2e55c08f32c861a281f4edfd5cc
Ben Groseclose <ben.groseclose01@gmail.com>
2022-07-23 12:03:19 -0400

    allow any origin

--- code/backend/BenGroseclose.API/Program.cs
+++ code/backend/BenGroseclose.API/Program.cs
@@ -19,7 +19,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("ClientPermission", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000", "http://localhost:80", "https://bengroseclose.com", "https://bengroseclose-website-frontend.salmonwater-d1a7f0b1.eastus.azurecontainerapps.io/")
+            .AllowAnyOrigin()
+            //.WithOrigins("http://localhost:3000", "http://localhost:80", "https://bengroseclose.com", "https://bengroseclose-website-frontend.salmonwater-d1a7f0b1.eastus.azurecontainerapps.io/")
             .AllowAnyHeader()
             .AllowAnyMethod()
             //.AllowAnyOrigin() // TODO: Remove this
